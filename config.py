aba339ef7fe4078214698b3efe0d0a654ad2ddc0
Yuren Cai <chinayuren2022@gmail.com>
2026-02-09 16:50:11 +0800

    去掉不需要的缓存

--- config.py
+++ config.py
@@ -18,7 +18,6 @@ os.environ.pop('HTTPS_PROXY', None)
 # Default monitoring list
 STOCK_POOL = [
     '513180', # 恒生科技ETF
-    '601988', # 中国银行
     '600172', # 黄河旋风
     '300316', # 晶盛机电
     '002985', # 北摩高科
@@ -63,14 +62,14 @@ HISTORY_LEN = 19       # Keep last 19 snapshots (3 mins @ 10s interval)
 MONITOR_INTERVAL = 10    # Check every 10 seconds
 
 # Model 1: Rocket Launch (Opportunity)
-RISE_SPEED_THRESHOLD = 0.4    # 3-min rise > 2%
-VOL_RATIO_THRESHOLD = 1.0    # Volume Ratio > 2.5
-NET_INFLOW_THRESHOLD = 50   # Net inflow > 1000 wan (10 million)
+RISE_SPEED_THRESHOLD = 1.0    # 3-min rise > 2%
+VOL_RATIO_THRESHOLD = 1.5    # Volume Ratio > 2.5
+NET_INFLOW_THRESHOLD = 100   # Net inflow > 1000 wan (10 million)
 
 # Model 2: High Dive (Risk)
-DROP_SPEED_THRESHOLD = -0.4   # 3-min drop < -2%
-ASK_BID_RATIO_LIMIT = 1.0     # Ask vol / Bid vol > 5x (Wall of Ask)
-BREAK_SUPPORT_LINE = -0.5     # Drop below daily -3%
+DROP_SPEED_THRESHOLD = -1.0   # 3-min drop < -2%
+ASK_BID_RATIO_LIMIT = 1.5     # Ask vol / Bid vol > 5x (Wall of Ask)
+BREAK_SUPPORT_LINE = -1.0     # Drop below daily -3%
 
 # Model 3: Undercurrent (Accumulation)
 # (Logic: Small price change + Huge inflow)
